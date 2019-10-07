96d2675d130b08a7bcbb5718522dd1033c45c558
Raúl Guzmán <raulguzmanguerrero@outlook.com>
2019-10-07 15:57:40 -0500

    Maquetados v2
    Todos los maquetados de las secciones principales y todos (excepto dos) de las herramientas

---
+++ Maquetados/js/functions.js
@@ -0,0 +1,9 @@
+function goIndex(){
+	var backimg = document.getElementById("backimg");
+	var contenedor = document.getElementById("contenedor");
+
+	backimg.style.display="none";
+	contenedor.style.display="block";
+
+
+}
