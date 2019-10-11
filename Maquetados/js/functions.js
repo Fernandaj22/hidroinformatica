8a246f6e7af8e539cc411e31a4f838835edd2bed
Fernanda Juárez <mjuarez22@alumnos.uaq.mx>
2019-10-11 15:26:13 -0500

    new styles index

--- Maquetados/js/functions.js
+++
@@ -1,9 +0,0 @@
-function goIndex(){
-	var backimg = document.getElementById("backimg");
-	var contenedor = document.getElementById("contenedor");
-
-	backimg.style.display="none";
-	contenedor.style.display="block";
-
-
-}
--- js/functions.js
+++ js/functions.js
@@ -1,9 +1,29 @@
+$(document).ready(function(){
+    $("#menu").click(function(){
+    	$("#options").collapse('toggle');
+    	bg = document.getElementById("bg");
+		wrapper = document.getElementById("wrapper");
+
+		bg.style.display="block";
+		wrapper.style.display="none";
+    });
+});
+
+function goOptions(){
+	bg = document.getElementById("bg");
+	options = document.getElementById("options");
+	wrapper = document.getElementById("wrapper");
+
+	bg.style.display="block";
+	options.style.display="block";
+	wrapper.style.display="none";
+}
+
 function goIndex(){
 	var backimg = document.getElementById("backimg");
 	var contenedor = document.getElementById("contenedor");
 
 	backimg.style.display="none";
 	contenedor.style.display="block";
-
 
 }
