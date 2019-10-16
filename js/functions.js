623f17bc600cc346df20811d05bc7ed82c1b7ae6
Fernanda Juárez <mjuarez22@alumnos.uaq.mx>
2019-10-15 19:26:54 -0500

    navbar desk and movil

--- js/functions.js
+++ js/functions.js
@@ -1,22 +1,6 @@
-$(document).ready(function(){
-    $("#menu").click(function(){
-    	$("#options").collapse('toggle');
-    	bg = document.getElementById("bg");
-		wrapper = document.getElementById("wrapper");
-
-		bg.style.display="block";
-		wrapper.style.display="none";
-    });
-});
-
 function goOptions(){
-	bg = document.getElementById("bg");
 	options = document.getElementById("options");
-	wrapper = document.getElementById("wrapper");
-
-	bg.style.display="block";
-	options.style.display="block";
-	wrapper.style.display="none";
+	options.classList.toggle("ver")
 }
 
 function goIndex(){
