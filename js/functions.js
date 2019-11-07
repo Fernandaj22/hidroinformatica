a276c100f52d3e1b898a35ffd0f4cf4355512423
Fer <mjuarez22@alumnos.uaq.mx>
2019-11-07 15:33:54 -0600

    nosotros add participantes

--- js/functions.js
+++ js/functions.js
@@ -8,29 +8,17 @@ function goIndex(){
 }
 
 function more(){
-	foto_mini = document.querySelector(".foto-mini");
-	cv = document.querySelector(".cv");
-
 	foto = document.querySelector(".foto");
 	info = document.querySelector(".info");
 
-	foto_mini.style.display="block";
-	cv.style.display="block";
-
 	foto.style.display="none";
-	info.style.display="none";
+	info.style.display="block";
 }
 
 function less(){
-	foto_mini = document.querySelector(".foto-mini");
-	cv = document.querySelector(".cv");
-
 	foto = document.querySelector(".foto");
 	info = document.querySelector(".info");
 
-	foto_mini.style.display="none";
-	cv.style.display="none";
-
 	foto.style.display="block";
-	info.style.display="block";
+	info.style.display="none";
 }
